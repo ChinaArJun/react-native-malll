6476da4a9fe82419f1dbcc7b7b5481671cb8f6be
ArJun <arjun@ArJundeMacBook-Pro.local>
2019-08-31 20:05:50 +0800

    搭建测试环境

---
+++ src/Root.js
@@ -0,0 +1,6 @@
+import React from 'react';
+// import Navigation from './navigation';
+import Test from './pages/test/test';
+
+// export default () => <Navigation />;
+export default () => <Test />;
